adbb9853d13a340732817bcb52c4ec2530896735
pleasegivesource <bobg2488@gmail.com>
2021-03-09 08:42:58 -0800

    beep

---
+++ src/main/java/me/ethius/template/Template.java
@@ -0,0 +1,14 @@
+package me.ethius.template;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Template implements ModInitializer {
+
+    public static final String NAME = "Template ";
+    public static final String VERSION = "1.0-Snapshot ";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
